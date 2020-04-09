4517815e35e891a458e301f8cd6780748fd119c2
xuruiangel <1161638196@qq.com>
2020-04-09 20:48:56 +0800

    小可爱~~

---
+++ yiZhanDaoDi/miniprogram/pages/login/login.js
@@ -0,0 +1,87 @@
+// pages/login/login.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+    onTapDayWeather: function (e) {
+    wx.navigateTo({
+      url: '../index/index',
+    })
+      wx.login({
+        success: function (res) {
+          console.log(res.code)
+          //发送请求
+          wx.request({
+            url: 'http://127.0.0.1:8090/unionid', //接口地址
+            data: { code: res.code },
+            header: {
+              'content-type': 'application/json' //默认值
+            },
+            success: function (res) {
+              console.log(res.data)
+            }
+          })
+        }
+      })
+  }
+})
